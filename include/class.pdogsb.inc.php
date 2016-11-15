b0dd48fbfdfee37599b5708e360d5c65d033836a
groupe LAB <ielow.submarine@gmail.com>
2016-11-15 15:32:07 +0100

    modification sur le serveur

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -18,9 +18,9 @@
 
 class PdoGsb{   		
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=gsbapplifrais';   		
-      	private static $user='root' ;    		
-      	private static $mdp='' ;	
+      	private static $bdd='dbname=gsb_frais';   		
+      	private static $user='root';    		
+      	private static $mdp='AzertY!59';	
 		private static $monPdo;
 		private static $monPdoGsb=null;
 		
