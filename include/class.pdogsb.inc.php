5e744dd0c388361646a27cf2a531ca936f4d4271
Benoît <Benot@192.168.68.84>
2016-11-29 16:46:15 +0100

    "modification du nom de la table"

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -19,7 +19,7 @@
 class PdoGsb{
 
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=gsb_frais';
+      	private static $bdd='dbname=gsb';
       	private static $user='root' ;    		
       	//private static $mdp = 'pass';
       	//private static $bdd='dbname=gsb_frais';   		
