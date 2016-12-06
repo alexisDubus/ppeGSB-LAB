1f4fe7e33bc7cc005aab1ff499fd6af4517bed3f
Alexis Dubus <ielow.submarine@gmail.com>
2016-12-06 21:18:10 +0100

    construction partielle du CRUD de l'administrateur

---
+++ controleurs/c_admin.php
@@ -0,0 +1,37 @@
+<?php
+
+/* 
+ * Controleur du CRUD
+ */
+include("vues/v_sommaire.php");
+$action = $_REQUEST['action'];
+$idUtilisateur = $_SESSION['idUtilisateur'];
+$leMois = $_REQUEST['lstMois']; 
+$leRole = $_SESSION['role'];
+
+$lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idUtilisateur,$leMois);
+$lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idUtilisateur,$leMois);
+
+
+
+
+
+switch ($action)
+{
+    case 'majFraitForfait':{
+        
+		}
+		break;
+    case 'ajouterFraitForfait':{
+        
+		}
+		break;
+    case 'editerFraitForfait':{
+        
+		}
+		break;
+    case 'supprFraitForfait':{
+        
+		}
+		break;
+}
