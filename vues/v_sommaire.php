1884b97c4ccae205b48e78a637879487f8252b56
Alexis Dubus <ielow.submarine@gmail.com>
2017-03-02 11:48:26 +0100

    Rajout du bouton pour signaler un probléme

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -40,9 +40,10 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="doc/DocumentationUtilisateur.pdf">Documentation</a></li>
-        <li class="active"><a href="index.php?uc=gererFrais&action=saisirFraisForfaitisés">Saisir mes fiches de frais forfaitisés</a></li>
-        <li class="active"><a href="index.php?uc=gererFraisHorsForfaits&action=saisirFraisHorsForfait">Saisir mes fiches de frais hors forfait</a></li>
-        <li class="active"><a href="index.php?uc=etatFrais&action=selectionnerMois">Mes fiches de frais</a></li>       
+        <li class="active"><a href="index.php?uc=gererFrais&action=saisirFraisForfaitisés">Fiches de frais forfaitisés</a></li>
+        <li class="active"><a href="index.php?uc=gererFraisHorsForfaits&action=saisirFraisHorsForfait">Fiches de frais hors forfait</a></li>
+        <li class="active"><a href="index.php?uc=etatFrais&action=selectionnerMois">Mes fiches de frais</a></li>    
+	<li class="active"><a href="../glpi">Signaler un probléme</a></li> 
         <?php if ($_SESSION['role'] == 'Administrateur') { ?> 
         <li class="active"><a href="index.php?uc=menuCRUD&action=read">Menu CRUD</a></li> 
         <?php } ?>
