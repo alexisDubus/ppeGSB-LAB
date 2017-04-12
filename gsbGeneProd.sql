5a0c0174f65939ccef11fe3ae06366ae1c20a4d0
Alexis Dubus <ielow.submarine@gmail.com>
2017-04-12 10:36:48 +0200

    MAJ Fichier de génération

--- gsbGeneProd.sql
+++ gsbGeneProd.sql
@@ -389,6 +389,37 @@ ALTER TABLE `lignefraishorsforfait`
 --
 ALTER TABLE `utilisateur`
   ADD CONSTRAINT `role_ibfk_1` FOREIGN KEY (`idRole`) REFERENCES `role` (`id`);
+  
+  CREATE TRIGGER `increment_utilisateur_version_medecin` AFTER UPDATE ON `medecin`
+ FOR EACH ROW update utilisateur 
+set version = version + 1
+where id = old.idutilisateur;
+
+CREATE TRIGGER `increment_utilisateur_version_visite` AFTER UPDATE ON `visite`
+ FOR EACH ROW update utilisateur 
+set version = version + 1
+where id = old.idutilisateur;
+
+CREATE TRIGGER `increment_utilisateur_version_insert_visite` AFTER INSERT ON `visite`
+ FOR EACH ROW update utilisateur 
+set version = version + 1 
+where id = new.idutilisateur;
+
+CREATE TRIGGER `increment_utilisateur_version_delete_visite` AFTER DELETE ON `visite`
+ FOR EACH ROW update utilisateur 
+set version = version + 1
+where id = old.idutilisateur;
+
+CREATE TRIGGER `increment_utilisateur_version_delete_medecin` AFTER DELETE ON `medecin`
+ FOR EACH ROW update utilisateur 
+set version = version + 1 
+where id = old.idutilisateur;
+
+CREATE TRIGGER `increment_utilisateur_version_insert_medecin` AFTER INSERT ON `medecin`
+ FOR EACH ROW update utilisateur 
+set version = version + 1 
+where id = new.idutilisateur;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
