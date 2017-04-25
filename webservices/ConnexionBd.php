f73fe92eb5617591cf9e79a9d44505b45e9620a7
Marliere Léo <leomarliere@hotmail.fr>
2017-04-25 20:36:39 +0200

    Modif mineure

--- webservices/ConnexionBd.php
+++ webservices/ConnexionBd.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-	$bd=new PDO('mysql:host=127.0.0.1;dbname=gsb_frais;port=8889','root','root');
+	$bd=new PDO('mysql:host=127.0.0.1;dbname=gsb_frais;port=8889','lamp','AzertY!59');
 }
 catch(Exception $e){
 	die('Erreur : '.$e->getMessage());
