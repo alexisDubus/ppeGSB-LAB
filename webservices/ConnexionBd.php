dfc266441d5dac253edcf77496bc4e2e3fc5f3d4
Marliere Léo <leomarliere@hotmail.fr>
2017-04-25 19:58:31 +0200

    Modif mineure

--- webservices/ConnexionBd.php
+++ webservices/ConnexionBd.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-	$bd=new PDO('mysql:host=172.16.223.129;dbname=gsb_frais;port=80','administrateur','');
+	$bd=new PDO('mysql:host=127.0.0.1;dbname=gsb_frais;port=8889','administrateur','');
 }
 catch(Exception $e){
 	die('Erreur : '.$e->getMessage());
