c05a26083e51b8eafc62678862d9953aa44b4ca9
Marliere Léo <leomarliere@hotmail.fr>
2017-05-08 16:01:33 +0200

    Done

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -43,7 +43,7 @@
         <li class="active"><a href="index.php?uc=gererFrais&action=saisirFraisForfaitisés">Fiches de frais forfaitisés</a></li>
         <li class="active"><a href="index.php?uc=gererFraisHorsForfaits&action=saisirFraisHorsForfait">Fiches de frais hors forfait</a></li>
         <li class="active"><a href="index.php?uc=etatFrais&action=selectionnerMois">Mes fiches de frais</a></li>    
-	      <li class="active"><a href="172.16.8.12/glpi" target="_blank">Signaler un probléme</a></li>
+	      <li class="active"><a href="172.16.9.12/glpi" target="_blank">Signaler un probléme</a></li>
         <li class="active"><a href='GSB_Mobile/app-release.apk' >Télécharger l'application</a></li>
         <?php if ($_SESSION['role'] == 'Administrateur') { ?> 
         <li class="active"><a href="index.php?uc=menuCRUD&action=read">Menu CRUD</a></li> 
