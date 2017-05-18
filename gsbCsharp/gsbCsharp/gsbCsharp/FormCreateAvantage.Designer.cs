7673c2004e319fd2a0888e75d010972b21473d71
BenoitCavrois <benoitcavrois@hotmail.fr>
2017-05-18 16:09:18 +0200

    Ajout du formulaire de création d'un avantage

---
+++ gsbCsharp/gsbCsharp/gsbCsharp/FormCreateAvantage.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace gsbCsharp
+{
+    partial class FormCreateAvantage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCreateAvantage = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nom de l\'avantage :";
+            // 
+            // txtCreateAvantage
+            // 
+            this.txtCreateAvantage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCreateAvantage.Location = new System.Drawing.Point(261, 99);
+            this.txtCreateAvantage.Name = "txtCreateAvantage";
+            this.txtCreateAvantage.Size = new System.Drawing.Size(206, 31);
+            this.txtCreateAvantage.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(61, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormCreateAvantage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 369);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCreateAvantage);
+            this.Controls.Add(this.label1);
+            this.Name = "FormCreateAvantage";
+            this.Text = "GSB : Creation d\'un avantage";
+            this.Load += new System.EventHandler(this.FormCreateAvantage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCreateAvantage;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ gsbCsharp/gsbCsharp/gsbCsharp/FormCreateAvantage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Metier;
+
+namespace gsbCsharp
+{
+    public partial class FormCreateAvantage : Form
+    {
+        public FormCreateAvantage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String nom = Passerelle.Passerelle.checkValueIsCorrect(txtCreateAvantage.Text.ToString());
+            Avantage unAvantage= new Avantage(nom);
+            Passerelle.Passerelle.addAvantage(unAvantage);
+            txtCreateAvantage.Text = "";
+            
+        }
+
+        private void FormCreateAvantage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
