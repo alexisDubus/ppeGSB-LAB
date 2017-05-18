99f41e5a585e9c3836d785b6d20a9f0f4bc73369
BenoitCavrois <benoitcavrois@hotmail.fr>
2017-05-18 16:10:36 +0200

    Ajout de la classe métier Avantage

---
+++ gsbCsharp/gsbCsharp/Metier/Avantage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public class Avantage
+    {
+        private String nom;
+        private int id;
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public Avantage(String nom)
+        {
+
+            setNom(nom);
+        }
+
+        public void setNom(String nom)
+        {
+            this.Nom = nom;
+        }
+
+        public String getNom()
+        {
+            return this.Nom;
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+    }
+}
